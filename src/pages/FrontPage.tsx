f54dd56be8787560c76f56f7857b6ef399bddcfd
juuser1 <117024265+juuser1@users.noreply.github.com>
2026-05-20 07:21:37 +0000

    lisätty haavoittuva lodash

--- src/pages/FrontPage.tsx
+++ src/pages/FrontPage.tsx
@@ -1,7 +1,28 @@
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { useEffect, useState } from 'react';
+import _ from 'lodash';
 
 export default function FrontPage() {
+	const [results, setResults] = useState<string[]>([]);
+
+useEffect(() => {
+	const photos = [
+		{ id: 1, title: "Sunset", album: "Nature", likes: 42, featured: true },
+		{ id: 2, title: "City lights", album: "Urban", likes: 18, featured: false },
+		{ id: 3, title: "Mountain view", album: "Nature", likes: 35, featured: true },
+		{ id: 4, title: "Street art", album: "Urban", likes: 27, featured: false },
+		{ id: 5, title: "Waterfall", album: "Nature", likes: 53, featured: true },
+	];
+
+	// countBy / sumBy
+	const totalLikes = _.sumBy(photos, "likes");
+
+	setResults([
+		` ${totalLikes}`,
+	]);
+}, []);
+
 	return (
 		<Box
 			sx={{
@@ -15,6 +36,9 @@ export default function FrontPage() {
 			<Typography variant='h3' component='h1' gutterBottom>
 				{'Welcome to my photo gallery!'}
 			</Typography>
+			{results.map((line, i) => (
+				<Typography key={i} variant='body1'>Likes ❤️ {line}</Typography>
+			))}
 		</Box>
 	);
 }
